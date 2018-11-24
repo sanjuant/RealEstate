a2b37e9d53e990f4278e3d1276e067a4045d1742
Sorrow <sanjuan.thibaut@gmail.com>
2018-11-24 23:05:42 +0100

    feat: Pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,13 +24,14 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("biens", name="property.index")
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        $properties = $this->repository->findAllVisible();
+        $properties = $paginator->paginate($this->repository->findAllVisibleQuery(), $request->query->getInt('page', 1), 12);
 
-        dump($properties);
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
             'properties' => $properties
---
+++ src/DataFixtures/PropertyFixture.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Property;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PropertyFixture extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 100; $i++) {
+            $property = new Property();
+            $property->setTitle($faker->words(3, true))
+                ->setDescription($faker->text(400))
+                ->setSurface($faker->numberBetween(20, 320))
+                ->setRooms($faker->numberBetween(2, 10))
+                ->setBedrooms($faker->numberBetween(2, 10))
+                ->setFloor($faker->numberBetween(0, 15))
+                ->setHeat($faker->numberBetween(0, \count(Property::HEAT) - 1))
+                ->setPrice($faker->numberBetween(10000, 1000000))
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setPostalCode($faker->postcode)
+                ->setSold(false);
+            $manager->persist($property);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
